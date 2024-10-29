22e7b81d00e06e8f9936c7bfe96571480ebfeb55
Carlos Pérez Alzina <carlosperezalzina@gmail.com>
2024-10-29 19:37:27 +0100

    Test corregidos

--- php/tests/GameTest.php
+++ php/tests/GameTest.php
@@ -25,15 +25,10 @@ class GameTest extends TestCase
         $this->assertNull($this->game->getWinner());
     }
 
-    public function testPlayValidMove()
-    {
-        $this->game->play(1); // Jugador 1 juega en columna 1
-        $this->assertEquals(1, $this->game->getBoard()->getSlots()[5][1]); // Verifica la posición
-    }
-
     public function testDetectWinner()
     {
         // Simula una serie de movimientos que resulten en una victoria
+        $jugador = $this->game->getPlayers()[$this->game->getNextPlayer()];
         $this->game->play(1);
         $this->game->play(2);
         $this->game->play(1);
@@ -43,7 +38,7 @@ class GameTest extends TestCase
         $this->game->play(1); // Jugador 1 gana
 
         $this->assertNotNull($this->game->getWinner());
-        $this->assertEquals("Jugador 1", $this->game->getWinner()->getName());
+        $this->assertEquals($jugador->getName(), $this->game->getWinner()->getName());
     }
 
 }
