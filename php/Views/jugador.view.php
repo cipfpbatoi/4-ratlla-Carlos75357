de80c9ecd4c28a3b84943d9d095771b9f57e4d25
Carlos Pérez Alzina <carlosperezalzina@gmail.com>
2024-10-29 18:58:30 +0100

    Cambios de diseño

--- php/Views/jugador.view.php
+++ php/Views/jugador.view.php
@@ -4,28 +4,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Selecció de jugador i color de fitxes</title>
+    <link rel="stylesheet" href="jugador.css?v=<?php echo time(); ?>">
 </head>
 <body>
-<h1>Configuració del joc</h1>
 
-<form action="/index.php" method="POST">
-    <label for="name">Nom del jugador:</label>
-    <input type="text" id="name" name="name" placeholder="Escriu el teu nom" required>
-    <br><br>
+    <h1>Configuració del Joc</h1>
 
-    <label for="color">Tria el color de les fitxes:</label>
-    <select id="color" name="color" required>
-        <option value="">-- Selecciona un color --</option>
-        <option value="red">Roig</option>
-        <option value="blue">Blau</option>
-        <option value="yellow">Groc</option>
-        <option value="green">Verd</option>
-        <option value="black">Negre</option>
-    </select>
-    <br><br>
+    <form action="/index.php" method="POST">
+        <fieldset>
+            <legend>Dades del jugador</legend>
+            
+            <label for="name">Nom del jugador:</label>
+            <input type="text" id="name" name="name" placeholder="Escriu el teu nom" required>
 
-    <input type="submit" value="Començar el joc">
-</form>
+            <div class="color-options">
+                <p>Tria el color de les fitxes:</p>
+                <?php
+                $colors = [
+                    'red',
+                    'blue',
+                    'yellow',
+                    'green',
+                    'purple',
+                    'orange',
+                    'pink',
+                    'brown',
+                    'black',
+                    'turquoise',
+                    'maroon',
+                    'silver',
+                    'goldenrod',
+                ];
+                foreach ($colors as $color) : ?>
+                    <label>
+                        <input type="radio" name="color" value="<?= $color ?>" required>
+                        <span class="color-label <?= $color ?>"></span>
+                    </label>
+                <?php endforeach; ?>
+            </div>
+        </fieldset>
+
+        <button type="submit">Començar el joc</button>
+    </form>
 
 </body>
 </html>
